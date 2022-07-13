851a4ffc158311f6dc342937a29eee89a5530b48
udeaghad <dozie.udeagha@gmail.com>
2022-07-13 13:55:24 +0100

    implented Page Routing and design

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './pages.css';
+
+const HomePage = () => (
+  <div className="HomeContainer">
+    <h2>Welcome to our page!</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Integer porta tempor felis eu mollis. Mauris eget tristique massa.
+      Nullam id mi bibendum, pretium augue in, venenatis metus. Mauris elit mauris,
+      accumsan eget ultricies eget, consequat vel enim.
+      Nam vitae nisl accumsan, ornare nulla a, congue diam. Donec hendrerit turpis nulla,
+      pulvinar scelerisque sapien luctus a.
+      sodales sodales.
+    </p>
+    <br />
+    <p>
+      Quisque ac ultrices nunc. Curabitur risus augue, hendrerit at nulla in, aliquet tempus augue.
+      Mauris tincidunt neque id orci eleifend varius.Nullam at lacus facilisis, congue massa ut,
+      condimentum velit. Fusce ac suscipit nulla. Nulla condimentum turpis non mollis maximus.
+      Ut sodales risus eget lorem suscipit tempus. Nulla sed sagittis nulla. Interdum et malesuada
+      fames ac ante ipsum primis in faucibus.Cras non molestie est, eu consectetur ligula.
+
+    </p>
+  </div>
+);
+
+export default HomePage;
